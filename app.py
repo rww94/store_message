8f49c7b692398fd0763bec5bebeec85167d077b8
rww94 <973125857@qq.com>
2019-05-25 13:21:19 +0800

    wrao

--- app.py
+++ app.py
@@ -1,22 +1,47 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import os
+import tkinter.messagebox
+from tkinter import *
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html',MARKER_DISPLACEMENT_num=2,MARKER_PRESSURE_num=1)
+
+@app.route('/subfrom',methods=['POST'])
+def subfrom():
+	MARKER_DISPLACEMENT_num = int(request.form.get("MARKER_DISPLACEMENT_num"))
+	MARKER_PRESSURE_num = int(request.form.get("MARKER_PRESSURE_num"))
+	return render_template('index.html',MARKER_DISPLACEMENT_num=MARKER_DISPLACEMENT_num,MARKER_PRESSURE_num=MARKER_PRESSURE_num)
 
 @app.route('/store',methods=['POST'])
 def store():
 	res = {}
-	name = ['MODULE_CONTAINER','MODULE','METHOD','POSTPROCESSOR','SOLVER_TYPE','MATERIAL_TYPE','ELEMENT_TYPE','MESH_FILENAME','OUTPUT_FILENAME']
+	name = ['MODULE_CONTAINER','MODULE','METHOD','POSTPROCESSOR','SOLVER_TYPE','MATERIAL_TYPE','MAT_PARA','ELEMENT_TYPE','MESH_FILENAME',
+	'MARKER_DISPLACEMENT','MARKER_PRESSURE','OUTPUT_FILENAME']
 	for t in name:
-		res[t] = request.form.get(t)
+		if t =='MAT_PARA' or  t =='MARKER_DISPLACEMENT' or t == 'MARKER_PRESSURE':
+			res[t] = request.form.getlist(t)
+		else:
+			res[t] = request.form.get(t)
 	outfile = res['OUTPUT_FILENAME']
+	out_dir = os.path.dirname(outfile)
+	if not os.path.exists(out_dir):
+		os.makedirs(out_dir)
 	f = open(outfile,'w')
 	for k,v in res.items():
-		f.write(k+" = "+v+"\n")
+		if k == 'MAT_PARA' or k == 'MARKER_DISPLACEMENT' or k == 'MARKER_PRESSURE':
+			f.write(k+" = (")
+			for i in range(len(v)):
+				if i!= len(v)-1:
+					f.write(str(v[i])+",")
+				else:
+					f.write(str(v[i]))
+			f.write(")\n")
+		else:
+			f.write(k+" = "+v+"\n")
 	f.close()
 	return render_template("info.html",res = res)
 
